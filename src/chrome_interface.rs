2d3a5f0f5aeda7a4984b89a42be1364b7e189c43
Garret Thomson <g@sirsonic.com>
2023-02-08 18:25:46 -0500

    - add panic hook - add setting check default browser prefs (not enabled) - add context menu (ugly) - refactor/tidy up registry util modes

--- src/chrome_interface.rs
+++ src/chrome_interface.rs
@@ -134,19 +134,23 @@ impl ChromeInterface
         return new;
     }
 
+    fn prefs_path(profile_dir: &String) -> PathBuf
+    {
+        let local_app_data = env::var(LOCALAPPDATA).unwrap();
+        return PathBuf::from(local_app_data).join("Google").join("Chrome").join(profile_dir).join("Preferences");
+    }
+
     fn open_local_statefile_as_object(&self) -> Value
     {
         let statefile_reader = File::open(self.statefile_path.to_os_string()).unwrap();
         return serde_json::from_reader(statefile_reader).unwrap();
     }
 
-    pub fn get_lastused_profile(&self) -> Option<String>
+    fn open_prefs_as_object(profile_dir: &String) -> Value
     {
-        let local_statefile_obj = self.open_local_statefile_as_object();
-        // important to get as_str as going to_string will represent value with doublequotes
-        // (and then this "infects" the chrome statefile when you try and open a profile with it)
-        let last_used_profile = local_statefile_obj["profile"]["last_used"].as_str().unwrap();
-        return Some(String::from(last_used_profile));
+        let prefs_path = Self::prefs_path(profile_dir);
+        let prefs_reader = File::open(prefs_path.as_os_str().to_os_string()).unwrap();
+        return serde_json::from_reader(prefs_reader).unwrap();
     }
 
     pub fn populate_profile_entries(&mut self) -> bool
@@ -206,40 +210,19 @@ impl ChromeInterface
         let mut statefile_write = File::options().write(true).truncate(true).open(statefile_path.clone()).unwrap();
         statefile_write.write(local_statefile_changed.to_string().as_bytes()).unwrap();
     }
+    
+    pub fn set_chrome_default_browser_check(&self, profile_dir: &String, check: bool)
+    {
+        let mut prefs_obj = Self::open_prefs_as_object(profile_dir);
+        prefs_obj["browser"]["default_browser_setting_enabled"] = Value::from(check);
+
+        // todo, make statefile/prefs objects that open/close on new and drop
+        let prefs_path = Self::prefs_path(profile_dir);
+        let mut statefile_write = File::options().write(true).truncate(true).open(prefs_path.clone()).unwrap();
+        statefile_write.write(prefs_obj.to_string().as_bytes()).unwrap();
+    }
 }
 
-/*
-// todo, get chrome icon from here:
 
-let img_bytes = reqwest::blocking::get("...")?
-    .bytes()?;
-
-let image = image::load_from_memory(&img_bytes)?;
-
-"account_info": [
-    {
-      "account_id": "101562270597223606393",
-      "accountcapabilities": {
-        "accountcapabilities/g42tslldmfya": 1,
-        "accountcapabilities/gi2tklldmfya": 1,
-        "accountcapabilities/gu2dqlldmfya": 1,
-        "accountcapabilities/gu4dmlldmfya": 0,
-        "accountcapabilities/guydolldmfya": 0,
-        "accountcapabilities/guzdslldmfya": 0,
-        "accountcapabilities/haytqlldmfya": 1
-      },
-      "email": "krufty78@gmail.com",
-      "full_name": "Garret Thomson",
-      "gaia": "101562270597223606393",
-      "given_name": "Garret",
-      "hd": "NO_HOSTED_DOMAIN",
-      "is_supervised_child": 0,
-      "is_under_advanced_protection": false,
-      "last_downloaded_image_url_with_size": "https://lh3.googleusercontent.com/a/AEdFTp7nyh4xYCDWmOGLz8eyQfciQxIVTfQMmAv4KaIOHl8=s256-c-ns",
-      "locale": "en",
-      "picture_url": "https://lh3.googleusercontent.com/a/AEdFTp7nyh4xYCDWmOGLz8eyQfciQxIVTfQMmAv4KaIOHl8=s96-c"
-    }
-
- */
 
 
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@
 
 use std::os::windows::process::CommandExt;
 use std::process::Command;
+use std::process::exit;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::str::FromStr;
+use std::panic;
+use std::str;
 use log::LevelFilter;
 use log::{debug,warn,info,error};
 
@@ -17,6 +20,23 @@ mod registry_utils;
 use chrome_interface::ChromeInterface;
 
 const DETACHED_PROCESS: u32 = 0x00000008;
+
+type RegUtilityFn = fn() -> Result<(), registry::Error>;
+fn do_utility_and_exit(util_fn: RegUtilityFn)
+{
+    match util_fn()
+    {
+        Err(e) => error!("Error creating registry keys: {:?}", e),
+        _ => info!("Registry keys created."),
+    }
+
+    exit(0);
+}
+
+fn soft_panic(url: &String)
+{
+    open_url_in_chrome_and_exit(&url, &String::default());
+}
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
@@ -33,10 +53,16 @@ struct Args {
    #[arg(long, default_value = "")]
    url: String,
 
-   /// Url to open
+   /// Log level (error, warn, info, debug, trace)
    #[arg(long, default_value = "info")]
    log_level: String,
+
+    /// force ui to open
+    #[arg(long, default_value = "false")]
+    force_ui: bool,
 }
+
+static mut PANIC_URL: [u8;2048] = [0; 2048];
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 async fn main() {
@@ -44,100 +70,86 @@ async fn main() {
     let args = Args::parse();
     debug!("args: {:?}", args);
 
+    // register minimum nice behaviour for panics, just open the damn browser
+    unsafe { PANIC_URL[0..args.url.len()].copy_from_slice(args.url.as_bytes()); };
+    panic::set_hook(Box::new(|_| {
+        unsafe {
+            // there's probably a less hairy way of doing this, but I'm not rust ninja enough yet
+            let mut url_str = str::from_utf8(&PANIC_URL).unwrap();
+            url_str = &url_str[0..PANIC_URL.into_iter().position(|r| { r == 0 }).unwrap()];
+            open_url_in_chrome_and_exit(&String::from(url_str), &String::default());
+        }
+    }));
+
     match LevelFilter::from_str(args.log_level.as_str()) {
         Ok(x) => simple_logging::log_to_file("test.log", x).unwrap(),
         Err(x) => warn!("failed to set log level! logging off {:?}", x),
     }
 
     if args.create_keys {
-        match registry_utils::create_registry_keys()
-        {
-            Err(e) => error!("Error creating registry keys: {:?}", e),
-            _ => info!("Registry keys created."),
-        }
-        return;
+        do_utility_and_exit(registry_utils::create_registry_keys);
     } else if args.delete_keys {
-        match registry_utils::delete_registry_keys()
-        {
-            Err(e) => error!("Error deleting registry keys: {:?}", e),
-            _ => info!("Registry keys deleted."),
-        }
+        do_utility_and_exit(registry_utils::delete_registry_keys);
+    }
+    
+    let mut chrome = chrome_interface::ChromeInterface::new();
+    if !chrome.populate_profile_entries()
+    {
+        error!("couldn't get chrome profile(s)");
+        soft_panic(&args.url);
+    }
+
+    let device_state = DeviceState::new();
+    let keys: Vec<Keycode> = device_state.get_keys();
+    if !args.force_ui && !keys.contains(&Keycode::LControl)
+    {
+        open_url_in_chrome_and_exit(&args.url, &String::default());
         return;
-    } else {  
-        
-        let mut chrome = chrome_interface::ChromeInterface::new();
-        if !chrome.populate_profile_entries()
-        {
-            // a "panic" should at least open a dang browser
-            open_url_in_chrome(&args.url, &String::default());
-        }
+    }
 
-        let last_used_profile_name = match chrome.get_lastused_profile()
-        {
-            Some(x) => x,
-            None => {
-                "No profile found".to_string();
-                return;
-            },
-        };
-           
-        if !chrome.profile_entries.iter().any(|x| { return x.profile_directory == last_used_profile_name; })
-        {
-            warn!("last used profile {} not found in profile_names! exiting", last_used_profile_name);
-            return;
-        }
+    // design notes: 
+    // - be able to "pin" a profile
+    // if ctrl pressed
+    //  open UI
+    // else
+    //  if profile pinned
+    //   open in that one
+    //  else
+    //   open in last used
 
-        // todo: 
-        // - be able to "pin" a profile
-        // if ctrl pressed
-        //  open UI
-        // else
-        //  if profile pinned
-        //   open in that one
-        //  else
-        //   open in last used
+    let app_height = (chrome.profile_entries.len() as f32) * MyApp::BUTTON_SIZE + 30.0;
+    let app_width = MyApp::PROFILE_BUTTON_WIDTH + MyApp::BUTTON_SIZE * 2.0 + 30.0; // profile button + two buttons + margins (5px*3)
 
-        let ci_arcm = Arc::new(Mutex::new(chrome));
-
-        let profile_picture_fetch = ci_arcm.clone();
-        for entry in &mut profile_picture_fetch.lock().unwrap().profile_entries {
-            let profile_picture_borrow = entry.profile_picture.clone();
-            let profile_name = entry.profile_name.clone();
-            tokio::runtime::Handle::current().spawn(
-                async move
-                {
-                    let mut locked_picture = profile_picture_borrow.lock().await;
-                    let fetch_picture_result = locked_picture.get_picture().await;
-                    if fetch_picture_result.is_err() {
-                        warn!("error fetching picture for \"{}\": {}", profile_name, fetch_picture_result.err().unwrap());
-                    }
+    let ci_arcm = Arc::new(Mutex::new(chrome));
+    let profile_picture_fetch = ci_arcm.clone();
+    for entry in &mut profile_picture_fetch.lock().unwrap().profile_entries {
+        let profile_picture_shared = entry.profile_picture.clone();
+        let profile_name = entry.profile_name.clone();
+        tokio::runtime::Handle::current().spawn(
+            async move
+            {
+                let mut locked_picture = profile_picture_shared.lock().await;
+                let fetch_picture_result = locked_picture.get_picture().await;
+                if fetch_picture_result.is_err() {
+                    warn!("error fetching picture for \"{}\": {}", profile_name, fetch_picture_result.err().unwrap());
                 }
-            );
-        }
-        
-        
-        let device_state = DeviceState::new();
-        let keys: Vec<Keycode> = device_state.get_keys();
-        if !keys.contains(&Keycode::LControl)
-        {
-            open_url_in_chrome(&args.url, &last_used_profile_name);
-            return;
-        }
-
-        // actually run the app
-        let options = eframe::NativeOptions {
-            initial_window_size: Some(egui::vec2(255.0, 300.0)),
-            resizable: true,
-            decorated: false,
-            ..Default::default()
-        };
-
-        eframe::run_native(
-            "chrome picker",
-            options,
-            Box::new(|_cc| Box::new(MyApp {chrome_interface: ci_arcm, url: args.url })),
+            }
         );
     }
+
+    // actually run the app
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(app_width  + 200.0, app_height)),
+        resizable: false,
+        // decorated: false,
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "chrome picker",
+        options,
+        Box::new(|_cc| Box::new(MyApp {chrome_interface: ci_arcm, url: args.url })),
+    );
 }
 
 struct MyApp {
@@ -148,6 +160,7 @@ struct MyApp {
 impl MyApp
 {
     const BUTTON_SIZE: f32 = 30.0;
+    const PROFILE_BUTTON_WIDTH: f32 = 200.0;
 }
 
 impl eframe::App for MyApp {
@@ -180,7 +193,8 @@ impl eframe::App for MyApp {
                                     )
                                 });
             
-                                ui.image(texture, egui::vec2(MyApp::BUTTON_SIZE, MyApp::BUTTON_SIZE));
+                                let image = egui::Image::new(texture, egui::vec2(MyApp::BUTTON_SIZE, MyApp::BUTTON_SIZE));
+                                ui.add_sized(egui::vec2(MyApp::BUTTON_SIZE, MyApp::BUTTON_SIZE), image);
                             }
                         };
                     }
@@ -189,31 +203,50 @@ impl eframe::App for MyApp {
                 ui.with_layout(egui::Layout::top_down(egui::Align::TOP), |ui| {
                     for profile_entry in &self.chrome_interface.lock().unwrap().profile_entries {
                         let mut button = egui::Button::new(profile_entry.profile_name.clone());
-                        button = button.min_size(egui::vec2(200.0, MyApp::BUTTON_SIZE));
-                        if ui.add(button).clicked() {
-                            open_url_in_chrome(&self.url, &profile_entry.profile_directory.clone());
+                        if ui.add_sized(egui::vec2(200.0, MyApp::BUTTON_SIZE), button).clicked() {
+                            open_url_in_chrome_and_exit(&self.url, &profile_entry.profile_directory.clone());
                         }
                     }
                 });
+
+                ui.with_layout(egui::Layout::top_down(egui::Align::TOP), |ui| {
+                    for profile_entry in &self.chrome_interface.lock().unwrap().profile_entries {
+                        let cb = egui::ComboBox::from_id_source(profile_entry.profile_name.as_str()).width(100.0)
+                        .show_ui(ui, |ui| {
+                            ui.set_min_size(egui::vec2(150.0, 30.0));
+                            ui.button("set default".to_string());
+                            ui.button("turn off browser check".to_string());
+                        });
+
+                        /*
+                        let mut button = egui::Button::new("...".to_string());
+                        button = button.min_size(egui::vec2(MyApp::BUTTON_SIZE, MyApp::BUTTON_SIZE));
+                        if ui.add(button).clicked() {
+                            // todo, dropdown menu
+                        }
+                        */
+                    }
+                });
+
             });
         });
     }
 }
 
-fn open_url_in_chrome(url: &String, profile_name: &String)
+fn open_url_in_chrome_and_exit(url: &String, profile_name: &String)
 {
-    // println!("Open url: {}", url);
+    debug!("url: {}", url);
     let mut chrome_command_line: String = String::default();
     match registry_utils::get_chrome_exe()
     {
-        Err(e) => println!("Failed to get chrome open command: {}", e),
+        Err(e) => error!("failed to get chrome open command: {}", e),
         Ok(v) => chrome_command_line = v,
     }
 
     let mut chrome_command = Command::new(chrome_command_line);
     chrome_command.creation_flags(DETACHED_PROCESS);
 
-    if profile_name.is_empty() {
+    if !profile_name.is_empty() {
         chrome_command.arg(format!("--profile-directory={}", profile_name));
     }
     
@@ -221,7 +254,7 @@ fn open_url_in_chrome(url: &String, profile_name: &String)
 
     let chrome_command_child_result = chrome_command.spawn();
 
-    // println!("command: {:?}", chrome_command);
+    debug!("chrome command: {:?}", chrome_command);
 
     match chrome_command_child_result
     {
