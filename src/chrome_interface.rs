224788922156d3d9f4867f83c8d2575e122b7370
Garret Thomson <g@sirsonic.com>
2023-02-08 14:55:27 -0500

    - removed web fetching, using pngs in user data directories - use profile colors if no picture - mask profile pictures with circle - add logging - improve ui layout substantially

--- src/chrome_interface.rs
+++ src/chrome_interface.rs
@@ -2,59 +2,105 @@
 
 use std::env;
 use serde_json::{Value};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::ffi::{OsStr, OsString};
 use std::fs::File;
 use std::io::Write;
 use std::sync::Arc;
-use egui::{ColorImage};
+use egui::{ColorImage, Color32};
 use futures::lock::Mutex;
 
 const LOCALAPPDATA:&str = "LOCALAPPDATA";
 
 pub struct ChromeProfilePicture
 {
-    picture_url: String,
+    picture_filename: OsString,
     pub img: Option<ColorImage>,
     pub profile_texture: Option<egui::TextureHandle>,
+    pub profile_color: [u8; 4],
 }
 
 impl ChromeProfilePicture
 {
-    pub fn new(img_url: &String) -> Self
+    pub fn new(profile_dir: &String, img_filename: &OsString, profile_color: &[u8;4]) -> Self
     {
+        let local_app_data = env::var(LOCALAPPDATA).unwrap();
+        let mut filename_path = PathBuf::default();
+        if !img_filename.is_empty() {
+            filename_path = PathBuf::from(&local_app_data);
+            filename_path = filename_path.join("Google").join("Chrome").join("User Data").join(profile_dir).join(img_filename);
+        }
+
         ChromeProfilePicture {
-            picture_url: img_url.clone(),
+            picture_filename: OsString::from(filename_path.as_os_str()), 
             img: None,
-            profile_texture: None
+            profile_texture: None,
+            profile_color: profile_color.clone(),
         }
-    }
-
-    pub fn set_image_data(self: &mut Self, data: &Vec<u8>)
-    {
-        fn load_image_from_memory(image_data: &[u8]) -> Result<ColorImage, image::ImageError> {
-            let image = image::load_from_memory(image_data)?;
-            let size = [image.width() as _, image.height() as _];
-            let image_buffer = image.to_rgba8();
-            let pixels = image_buffer.as_flat_samples();
-            Ok(ColorImage::from_rgba_unmultiplied(
-                size,
-                pixels.as_slice(),
-            ))
-        }
-
-        self.img = Some(load_image_from_memory(data).unwrap_or_default());
     }
 }
 
 impl ChromeProfilePicture {
-    pub async fn get_picture(self: &mut Self) -> Result<(), reqwest::Error>
+
+    fn apply_circle_mask(self: &mut Self)
     {
-        if !self.picture_url.is_empty()
-        {
-            let my_result = reqwest::get(self.picture_url.clone()).await?.bytes().await?;
-            self.set_image_data(&my_result.to_vec());
+        if self.img.is_none() {
+            return;
         }
+
+        let img_ref = self.img.as_mut().unwrap();
+        assert_eq!(img_ref.width(), img_ref.height(), "not square!");
+        let dim = img_ref.width() as i32;
+        let half_dim = (img_ref.width()/2) as i32;
+        let mut x: i32 = 0;
+        let mut y: i32 = 0;
+        for pixel in &mut img_ref.pixels
+        {
+            let x_from_mid = x - half_dim;
+            let y_from_mid = y - half_dim;
+            let mut dist = (x_from_mid.pow(2)+ y_from_mid.pow(2)) as f32;
+            dist = dist.sqrt();
+            
+            if dist > (half_dim as f32)
+            {
+                *pixel = Color32::from_rgba_unmultiplied(pixel.r(), pixel.g(), pixel.b(), 0).clone();
+            }
+
+            if x == 0
+            {
+                y += 1;
+            }
+
+            x += 1;
+            x = x%dim;
+        }
+    }
+    pub async fn get_picture(self: &mut Self) -> Result<(), image::ImageError>
+    {
+        if !self.picture_filename.is_empty()
+        {
+            fn load_image_from_path(path: &std::path::Path) -> Result<Option<egui::ColorImage>, image::ImageError> {
+                let image = image::io::Reader::open(path)?.decode()?;
+                let size = [image.width() as _, image.height() as _];
+                let image_buffer = image.to_rgba8();
+                let pixels = image_buffer.as_flat_samples();
+                Ok(Some(egui::ColorImage::from_rgba_unmultiplied(
+                    size,
+                    pixels.as_slice(),
+                )))
+            }
+
+            self.img = load_image_from_path(Path::new(&self.picture_filename)).unwrap();
+        } else {
+            // make an image entirely with the color of the profile
+            let image_size = 128;
+            let red_pixels = self.profile_color.iter().cloned().cycle();
+            let image_array = Vec::from(red_pixels.take(image_size * image_size * self.profile_color.len()).collect::<Vec<u8>>());
+            self.img = Some(ColorImage::from_rgba_unmultiplied([image_size, image_size], &image_array));
+        }
+
+        self.apply_circle_mask();
+
         return Ok(());
     }
 }
@@ -112,15 +158,30 @@ impl ChromeInterface
             for profile_entry in json_profiles.as_object().unwrap()
             {   
                 let entry_data = profile_entry.1.as_object().unwrap();
-                let mut profile_picture_url = String::default();
-                if entry_data.get_key_value("last_downloaded_gaia_picture_url_with_size").is_some() {
-                    profile_picture_url = String::from(entry_data["last_downloaded_gaia_picture_url_with_size"].as_str().unwrap());
+                let mut profile_filename = OsString::default();
+                if entry_data.get_key_value("gaia_picture_file_name").is_some() {
+                    profile_filename = OsString::from(entry_data.get_key_value("gaia_picture_file_name").unwrap().1.as_str().unwrap_or_default());
                 }
 
+                let mut profile_color: [u8; 4] = [0,0,0,0];
+                if entry_data.get_key_value("default_avatar_fill_color").is_some() {
+                    let colour = entry_data.get_key_value("default_avatar_fill_color").unwrap().1.as_i64().unwrap_or_default();
+                    // color is argb
+                    profile_color[3] = ((colour >> 24) & 0xff) as u8;
+                    profile_color[0] = ((colour >> 16) & 0xff) as u8;
+                    profile_color[1] = ((colour >> 8) & 0xff) as u8;
+                    profile_color[2] = ((colour >> 0) & 0xff) as u8;
+                }
+         
                 let chrome_profile_entry: ChromeProfileEntry = ChromeProfileEntry {
                     profile_directory: profile_entry.0.to_string(),
                     profile_name: String::from(entry_data["shortcut_name"].as_str().unwrap()),
-                    profile_picture: Arc::new(Mutex::new(ChromeProfilePicture::new(&profile_picture_url)))
+                    profile_picture: Arc::new(Mutex::new(
+                        ChromeProfilePicture::new(
+                        &profile_entry.0.to_string(),
+                        &profile_filename,
+                        &profile_color)
+                    )),
                 };
 
                 self.profile_entries.push(chrome_profile_entry);
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ use std::os::windows::process::CommandExt;
 use std::process::Command;
 use std::sync::Arc;
 use std::sync::Mutex;
+use std::str::FromStr;
+use log::LevelFilter;
+use log::{debug,warn,info,error};
 
 use eframe::egui;
-use egui::{TextureOptions, TextureFilter, ImageData, ColorImage};
 use device_query::{DeviceQuery, DeviceState, Keycode};
 use clap::Parser;
 
@@ -30,26 +32,35 @@ struct Args {
    /// Url to open
    #[arg(long, default_value = "")]
    url: String,
+
+   /// Url to open
+   #[arg(long, default_value = "info")]
+   log_level: String,
 }
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 async fn main() {
 
     let args = Args::parse();
-    println!("args: {:?}", args);
+    debug!("args: {:?}", args);
+
+    match LevelFilter::from_str(args.log_level.as_str()) {
+        Ok(x) => simple_logging::log_to_file("test.log", x).unwrap(),
+        Err(x) => warn!("failed to set log level! logging off {:?}", x),
+    }
 
     if args.create_keys {
         match registry_utils::create_registry_keys()
         {
-            Err(e) => println!("Error creating registry keys: {:?}", e),
-            _ => println!("Registry keys created."),
+            Err(e) => error!("Error creating registry keys: {:?}", e),
+            _ => info!("Registry keys created."),
         }
         return;
     } else if args.delete_keys {
         match registry_utils::delete_registry_keys()
         {
-            Err(e) => println!("Error deleting registry keys: {:?}", e),
-            _ => println!("Registry keys deleted."),
+            Err(e) => error!("Error deleting registry keys: {:?}", e),
+            _ => info!("Registry keys deleted."),
         }
         return;
     } else {  
@@ -72,32 +83,39 @@ async fn main() {
            
         if !chrome.profile_entries.iter().any(|x| { return x.profile_directory == last_used_profile_name; })
         {
-            println!("last used profile {} not found in profile_names!", last_used_profile_name);
+            warn!("last used profile {} not found in profile_names! exiting", last_used_profile_name);
             return;
         }
 
         // todo: 
-        // - surpress default browser warning (Chrome/Default/Preferences: check_default_browser)
-        // - enumerate all (chromium?) browsers and handle openers programmatically?
-    
+        // - be able to "pin" a profile
+        // if ctrl pressed
+        //  open UI
+        // else
+        //  if profile pinned
+        //   open in that one
+        //  else
+        //   open in last used
+
         let ci_arcm = Arc::new(Mutex::new(chrome));
 
         let profile_picture_fetch = ci_arcm.clone();
         for entry in &mut profile_picture_fetch.lock().unwrap().profile_entries {
             let profile_picture_borrow = entry.profile_picture.clone();
+            let profile_name = entry.profile_name.clone();
             tokio::runtime::Handle::current().spawn(
                 async move
                 {
                     let mut locked_picture = profile_picture_borrow.lock().await;
                     let fetch_picture_result = locked_picture.get_picture().await;
                     if fetch_picture_result.is_err() {
-                        println!("error fetching picture: {}", fetch_picture_result.err().unwrap());
+                        warn!("error fetching picture for \"{}\": {}", profile_name, fetch_picture_result.err().unwrap());
                     }
                 }
             );
         }
         
-        /* 
+        
         let device_state = DeviceState::new();
         let keys: Vec<Keycode> = device_state.get_keys();
         if !keys.contains(&Keycode::LControl)
@@ -105,25 +123,26 @@ async fn main() {
             open_url_in_chrome(&args.url, &last_used_profile_name);
             return;
         }
-        */
 
         // actually run the app
         let options = eframe::NativeOptions {
-            initial_window_size: Some(egui::vec2(50.0, 50.0)),
-            resizable: false,
+            initial_window_size: Some(egui::vec2(255.0, 300.0)),
+            resizable: true,
+            decorated: false,
             ..Default::default()
         };
 
         eframe::run_native(
-            "Chromefilla",
+            "chrome picker",
             options,
-            Box::new(|_cc| Box::new(MyApp {chrome_interface: ci_arcm })),
+            Box::new(|_cc| Box::new(MyApp {chrome_interface: ci_arcm, url: args.url })),
         );
     }
 }
 
 struct MyApp {
     chrome_interface: Arc<Mutex<ChromeInterface>>,
+    url: String,
 }
 
 impl MyApp
@@ -145,14 +164,6 @@ impl eframe::App for MyApp {
                 
                 ui.with_layout(egui::Layout::top_down(egui::Align::TOP), |ui| {
                     for profile_entry in &self.chrome_interface.lock().unwrap().profile_entries {
-                        let mut button = egui::Button::new(profile_entry.profile_directory.clone());
-                        button = button.min_size(egui::vec2(200.0, MyApp::BUTTON_SIZE));
-                        ui.add(button);
-                    }
-                });
-
-                ui.with_layout(egui::Layout::top_down(egui::Align::TOP), |ui| {
-                    for profile_entry in &self.chrome_interface.lock().unwrap().profile_entries {
 
                         let mut lock = profile_entry.profile_picture.try_lock();
                         if let Some(ref mut _mutex) = lock {
@@ -171,17 +182,17 @@ impl eframe::App for MyApp {
             
                                 ui.image(texture, egui::vec2(MyApp::BUTTON_SIZE, MyApp::BUTTON_SIZE));
                             }
-                            else {
-                                let texture: &egui::TextureHandle = profile_picture.profile_texture.get_or_insert_with(|| {
-                                    // Load the texture only once.
-                                    ui.ctx().load_texture(
-                                        format!("{} Profile Pic Texture", profile_entry.profile_name),
-                                        ColorImage::from_rgba_unmultiplied([1, 1], &[1, 0, 0, 1]),
-                                        Default::default()
-                                    )
-                                });
-                            }
                         };
+                    }
+                });
+
+                ui.with_layout(egui::Layout::top_down(egui::Align::TOP), |ui| {
+                    for profile_entry in &self.chrome_interface.lock().unwrap().profile_entries {
+                        let mut button = egui::Button::new(profile_entry.profile_name.clone());
+                        button = button.min_size(egui::vec2(200.0, MyApp::BUTTON_SIZE));
+                        if ui.add(button).clicked() {
+                            open_url_in_chrome(&self.url, &profile_entry.profile_directory.clone());
+                        }
                     }
                 });
             });
@@ -191,7 +202,7 @@ impl eframe::App for MyApp {
 
 fn open_url_in_chrome(url: &String, profile_name: &String)
 {
-    println!("Open url: {}", url);
+    // println!("Open url: {}", url);
     let mut chrome_command_line: String = String::default();
     match registry_utils::get_chrome_exe()
     {
@@ -210,7 +221,7 @@ fn open_url_in_chrome(url: &String, profile_name: &String)
 
     let chrome_command_child_result = chrome_command.spawn();
 
-    println!("command: {:?}", chrome_command);
+    // println!("command: {:?}", chrome_command);
 
     match chrome_command_child_result
     {
